50d206a08a5e3e9fafaf3c537c28ca5e752991a7
Joshua Spears <j.dspears@yahoo.com>
2018-03-07 21:08:50 -0800

    Trip, and Location models created and synced to db.

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 // DEPENDENCIES
 const express = require("express"),
-        bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
 
-        // DB MODELS
-// const db = require("./app_api/models");
-// PORT
-const PORT = process.env.PORT || 3001;
-const app = express();
+    // DB MODELS
+    db = require("./app_api/models"),
+
+    // PORT
+    PORT = process.env.PORT || 3001,
+    app = express();
+
 // ROUTES FILE CONTAINING OUR ROUTES
-// const routes = require("./app_api/routes/employee.route");
+// const routes = require("./app_api/routes/");
 
-// // BODY PARSER CONFIG
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(express.static('app_client/build'));
+// BODY PARSER CONFIG
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('app_client/build'));
 
 // API ROUTES
 // app.use("/api", routes);
 
-// // SYNC THE SQL DB AND THEN LISTEN TO PORT
-// db.sequelize.sync({ force: true })
-//     .then(() => {
+// SYNC THE SQL DB AND THEN LISTEN TO PORT
+db.sequelize.sync({ force: true })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Listening on PORT: ${PORT}`);
+        }); // END THEN 
+    }); // END APP LISTEN
 
-//     }); // END APP LISTEN
-
-app.listen(PORT, () => {
-    console.log(`Listening on PORT: ${PORT}`);
-}); // END THEN 
