9be905d0ad6569f2c25cc422d93129332c71b088
Rey Cruz <reycruz@Reys-MBP.attlocal.net>
2018-03-11 22:01:51 -0700

    search form search.js -pages functionality coded

--- app_client/src/pages/Search/Search.js
+++ app_client/src/pages/Search/Search.js
@@ -3,12 +3,49 @@ import './Search.css';
 import SearchForm from '../../components/SearchForm'
 
 class Search extends Component {
-render() {
-    return(
-        <div>
-            <SearchForm />
-        </div>
-        )
+    constructor(props) {
+        super(props);
+        this.state = {
+            nationalParkSearch: '',
+
+        };
+        this.validateSearch = this.validateSearch.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handlenationalParkSearch = this.handlenationalParkSearch.bind(this);
+
+    };
+
+
+    validateSearch(input) {
+        return input !== '';
+    }
+
+    handleSearch(input) {
+        if (!this.validateSearch(input)) {
+            alert('Please enter a National Park');
+        } else {
+            alert(input);
+        }
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
+    handlenationalParkSearch(input) {
+        this.handleSearch(input)
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchForm
+                    nationalParkSearch={this.state.nationalParkSearch}
+                />
+            </div>
+        );
     };
 };
 
