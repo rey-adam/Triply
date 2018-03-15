45fc4813a01006754bf8da088de3bf1081194b4a
Rey Cruz <reycruz@Reys-MBP.attlocal.net>
2018-03-14 22:33:49 -0700

    added accordion component

---
+++ app_client/src/components/Accordion/Accordion.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Accordion.css';
+
+class Accordion extends Component {
+    render() {
+        return (
+            
+        );
+    };
+};
+
+export default Accordion;
