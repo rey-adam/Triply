b2a5573c88e5e04c7ca782da340f82079e215836
Jason <vivanaranja@gmail.com>
2018-03-17 15:09:37 -0700

    map container

--- app_client/src/pages/MapContainer/MapContainer.js
+++ app_client/src/pages/MapContainer/MapContainer.js
@@ -1,25 +1,44 @@
 //import the GoogleApiWrapper
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-import Map from '../../components/Map/Map';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+// import Map from '../../components/Map/Map';
 
-
+const style = {
+    width: '90vw', // 90vw basically means take up 90% of the width screen. px also works.
+    height: '75vh',
+    
+}
 
 
 class MapContainer extends Component {
 
     render() {
-        const style = { // MUST specify dimensions of the Google map or it will not work. Also works best when style is specified inside the render function and created as an object
-            width: '90vw', // 90vw basically means take up 90% of the width screen. px also works.
-            height: '75vh' // 75vh similarly will take up roughly 75% of the height of the screen. px also works.
-        }
+        
 
         return ( // in our return function you must return a div with ref='map' and style.
             <div style={style}>
-                <Map google={this.props.google} />
+                <Map 
+                google={this.props.google}
+                zoom={14}
+                // mapTypeId={terrain}
+                style={style}
+                initialCenter={{
+                lat: 37.8651,
+                lng: -119.5383
+                }}
+                
+                        
+                
+                
+                
+                
+                
+                
+                
+                />
                 loading map...
-      </div>
+            </div>
         )
     };
 };
