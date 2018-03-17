34b6087a77471d2987e7a32d8c047f0a6be1e74b
Rey Cruz <reycruz@dhcp-v099-097.mobile.uci.edu>
2018-03-17 14:09:13 -0700

    Dashboard layout testing

--- app_client/src/pages/Dashboard/Dashboard.js
+++ app_client/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
+import Navbar from '../../components/Navbar';
 // import the Google Maps API Wrapper from google-maps-react
 import { GoogleApiWrapper } from 'google-maps-react';
 import MapContainer from '../MapContainer/MapContainer';
@@ -8,8 +9,89 @@ class Dashboard extends Component {
     render() {
         return (
             <div>
-            <h1>Triply Dashboard</h1>
+                <Navbar />
+
+                {/* create new trip form */}
+            <div id="tripName">
+                <form id="input">
+                    <div className="form-group">
+                        <label for="formGroupExampleInput">Create new trip</label>
+                        <input type="text" className="" id="" placeholder="Trip name..." />
+                        <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                    </div>
+                </form>
+            </div>
+
+            {/* Saved Trips  */}
+            <div>
+            <ul className="list-group">
+            <li className="list-group-item"><h1>Saved Trips</h1></li>
+            <li className="list-group-item">Bryce</li>
+
+            <li className="list-group-item">Glacier</li>
+            <li className="list-group-item">Yellowstone</li>
+            <li className="list-group-item">Yosemite</li>
+            <li className="list-group-item">Zion</li>
+          </ul>
+            </div>
+
+            {/* Saved Trips  2 */}
+            <div id="accordion">
+        <div className="card">
+            <div className="card-header" id="headingOne">
+            <h5 className="mb-0">
+                <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Collapsible Group Item #1
+                </button>
+            </h5>
+            </div>
+
+            <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div className="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="card-header" id="headingTwo">
+            <h5 className="mb-0">
+                <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Collapsible Group Item #2
+                </button>
+            </h5>
+            </div>
+            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div className="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="card-header" id="headingThree">
+            <h5 className="mb-0">
+                <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Collapsible Group Item #3
+                </button>
+            </h5>
+            </div>
+            <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+            <div className="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+            </div>
+        </div>
+        </div>
+
+            {/* weather div  */}
+            <div>
+
+            </div>
+            
+            {/*  */}
+            <h5>Triply Dashboard</h5>
                 <MapContainer google={this.props.google}/>
+
+                
             </div>
 
         );
