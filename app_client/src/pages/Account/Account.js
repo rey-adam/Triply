e23805ad02d3cdd9110a9abab6c18b2269a9d513
MWC <chzwzrd@gmail.com>
2018-03-20 16:26:28 -0700

    set up initial account page

--- app_client/src/pages/Account/Account.js
+++ app_client/src/pages/Account/Account.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from '../../components/Navbar';
+import AccountForm from '../../components/AccountForm';
 import './Account.css';
 
 class Account extends Component {
     render() {
         return (
-            <div>
+            <div className="account-div">
                 <Navbar />
+                <AccountForm />
             </div>
         );
     };
